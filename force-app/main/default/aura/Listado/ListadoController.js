cadb805555e5585b2734c73db01e0dccd5fd4380
unknown <samuelmoralesartal@gmail.com>
2021-02-23 12:50:42 +0100

    funciona el listado pero con 5 if y saca el listado guardando el sitio de los caracteres anteriores

--- force-app/main/default/aura/Listado/ListadoController.js
+++ force-app/main/default/aura/Listado/ListadoController.js
@@ -1,5 +1,5 @@
 ({
-    addToTable : function(component, event, helper) {
+    addToTable: function (component, event, helper) {
         var input1 = event.getParam("nombreComercial");
         var input2 = event.getParam("nombreCliente");
         var input3 = event.getParam("matriculaVenta");
@@ -7,16 +7,58 @@
         var input5 = event.getParam("precioVenta");
 
         console.log("recibiendo evento");
-       if(input1, input2, input3, input4, input5){
-           var list = component.get("v.output");
-           var objeto1 = {nombreComercial: input1}
-           var objeto2 = {nombreCliente: input2}
-           var objeto3 = {matriculaVenta: input3}
-           var objeto4 = {fechaVenta: input4}
-           var objeto5 = {precioVenta: input5}
-           list.push(objeto1, objeto2, objeto3, objeto4, objeto5);
-           component.set("v.output",list);
-       }
-       console.log(component.get("v.output"));
-   }
+        console.log(input1, input2, input3, input4, input5);
+        //listado objeto 1
+        if (input1) {
+            var list = component.get("v.output");
+            var objeto1 = { nombreComercial: input1 }
+            list.push(objeto1);
+            component.set("v.output", list);
+        }
+        console.log(component.get("v.output"));
+        //listado objeto 2
+        if (input2) {
+            var list = component.get("v.output");
+            var objeto2 = { nombreCliente: input2 }
+            list.push(objeto2);
+            component.set("v.output", list);
+        }
+        console.log(component.get("v.output"));
+        //listado objeto 3
+        if (input3) {
+            var list = component.get("v.output");
+            var objeto3 = { matriculaVenta: input3 }
+            list.push(objeto3);
+            component.set("v.output", list);
+        }
+        console.log(component.get("v.output"));
+        //listado objeto 4
+        if (input4) {
+            var list = component.get("v.output");
+            var objeto4 = { fechaVenta: input4 }
+            list.push(objeto4);
+            component.set("v.output", list);
+        }
+        console.log(component.get("v.output"));
+        //listado objeto 5
+        if (input5) {
+            var list = component.get("v.output");
+            var objeto5 = { precioVenta: input5 }
+            list.push(objeto5);
+            component.set("v.output", list);
+        }
+        console.log(component.get("v.output"));
+    }
 })
+/*addToTable : function(component, event, helper) {
+    var input = event.getParam("input");
+    console.log("recibiendo evento");
+    console.log(input);
+   if(input){
+       var list = component.get("v.output");
+       var objeto = {nombre: input, apellido: "Doe"}
+       list.push(objeto);
+       component.set("v.output",list);
+   }
+   console.log(component.get("v.output"));
+},*/
