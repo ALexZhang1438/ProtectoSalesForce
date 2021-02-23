fb3994f0a83cedf59b4e982d1774fe3faad4575a
unknown <samuelmoralesartal@gmail.com>
2021-02-23 13:05:25 +0100

    ahora va el listado con sus defectos

--- force-app/main/default/aura/Listado/ListadoController.js
+++ force-app/main/default/aura/Listado/ListadoController.js
@@ -7,7 +7,6 @@
         var input5 = event.getParam("precioVenta");
 
         console.log("recibiendo evento");
-<<<<<<< HEAD
         console.log(input1, input2, input3, input4, input5);
         //listado objeto 1
         if (input1) {
@@ -63,28 +62,3 @@
    }
    console.log(component.get("v.output"));
 },*/
-=======
-        if (input1, input2, input3, input4, input5) {
-            var list = component.get("v.output");
-
-            var objeto1 = { nombreComercial: input1 }
-            var objeto2 = { nombreCliente: input2 }
-            var objeto3 = { matriculaVenta: input3 }
-            var objeto4 = { fechaVenta: input4 }
-            var objeto5 = { precioVenta: input5 }
-
-            list.push(objeto1, objeto2, objeto3, objeto4, objeto5);
-            component.set("v.output1", list);
-            component.set("v.output2", list);
-            component.set("v.output3", list);
-            component.set("v.output4", list);
-            component.set("v.output5", list);
-        }
-        console.log(component.get("v.output1"));
-        console.log(component.get("v.output2"));
-        console.log(component.get("v.output3"));
-        console.log(component.get("v.output4"));
-        console.log(component.get("v.output5"));
-    }
-})
->>>>>>> aaf74144be8e85264ece951b949e3cbcfcc4e22b
